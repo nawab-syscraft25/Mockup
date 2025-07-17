f14106f88efdf93f8b8f845a6e2b75b8a86997aa
nawab-syscraft25 <nawab.syscraft@gmail.com>
2025-07-17 13:16:11 +0000

    Add hashRout

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import Whitepaper from './Whitepaper'
-import ToS from './ToS'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import App from './App'
+// import Whitepaper from './Whitepaper'
+// import ToS from './ToS'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route exact path='/whitepaper' element={<Whitepaper />} />
+//         <Route exact path='/tos' element={<ToS />} />
+//         <Route path='/' element={<App />}/>
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Whitepaper from './Whitepaper';
+import ToS from './ToS';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route exact path='/whitepaper' element={<Whitepaper />} />
-        <Route exact path='/tos' element={<ToS />} />
-        <Route path='/' element={<App />}/>
+        <Route path="/whitepaper" element={<Whitepaper />} />
+        <Route path="/tos" element={<ToS />} />
+        <Route path="/" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
